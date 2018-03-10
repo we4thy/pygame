0b2248c6960f38972a8a8ee93e71356db97211f0
we4thy <we4thyWoutlook.com>
2018-03-11 00:28:26 +0800

    add ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     pygame.init()
@@ -11,10 +12,11 @@ def run_game():
         (ai_settings.screen_width,ai_settings.screen_height)
     )
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (230, 230, 230)
+    ship = Ship(screen)
     running = True
-    while running:
+    while True:
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
